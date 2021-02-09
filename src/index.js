318b773ca53121a241f2e5ac4dcc0077c3442887
Valeria Isaeva <valevs.isv@gmail.com>
2021-02-09 16:00:15 +0300

    add react structure

--- src/index.js
+++ src/index.js
@@ -1,27 +1,21 @@
-import {createStore, bindActionCreators} from 'redux';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import reducer from './reducer';
-import * as actions from './actions';
+import App from './components/counter';
 
 
 
 const store = createStore(reducer);
-const {dispatch} = store;
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>
+    , document.getElementById('root'));
 
 
-const {inc, dec, rnd} = bindActionCreators(actions, dispatch);
 
-
-document.getElementById('inc').addEventListener('click', inc);
-document.getElementById('dec').addEventListener('click', dec);
-document.getElementById('rnd').addEventListener('click', () => {
-  const value = Math.floor(Math.random() * 10);
-  rnd(value);
-});
-
-const update = () => {
-  document.getElementById('counter').textContent = store.getState();
-}
-
-store.subscribe(update);
 
 
